876d19a4c27665acec48f7f1868f06ca3e783fcc
Sven <svkanter@gmail.com>
2024-10-27 01:04:52 +0200

    wuhu - erste Fragen und Antworten laufen, 1:n question zu answers mittels related_name im ORM

--- quiz_project/poll_app/views.py
+++ quiz_project/poll_app/views.py
@@ -7,6 +7,7 @@ def ask_new_question(request):
     name = 'Ben Hanter'
     wochentag = datetime.now().strftime('%A')
     zahl = random.randint(1,100)
-    all_questions = Question.objects.all()
-    q_text = all_questions[random.randint(1,len(all_questions)-1)]
-    return render(request, 'index.html', {'wochentag' : wochentag, 'name' : name, 'zahl' : zahl , 'question' : q_text })
+    #all_questions = Question.objects.all()
+    all_questions = Question.objects.prefetch_related('answers')
+    question = all_questions[random.randint(1,len(all_questions)-1)]
+    return render(request, 'index.html', {'wochentag' : wochentag, 'name' : name, 'zahl' : zahl , 'question' : question })
